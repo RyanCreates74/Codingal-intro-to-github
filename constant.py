f02a95313c01e86d7e9f47ee09557737d4aca1ca
RyanCreates74 <uppal.ryan@gmail.com>
2024-12-11 18:10:04 +0000

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by the user is:", n)
+    iteration += 1
+    print("Toatal iterations done by code is:", iteration)
+
+printnumber(10)
+printnumber(20)
+
+print("With any 'n' times taken, our code won't change.")
